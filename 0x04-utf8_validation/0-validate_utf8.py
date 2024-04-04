605df4d24afb8b92198b437e759637c668b070d1
Mikenziokah <mikenziokah@gmail.com>
2024-04-04 12:09:00 -0700

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,37 +2,29 @@
 """ a method that detrmines if a given dataset represents a UTF-8 encording
 """
 def validUTF8(data):
-    """ function to validate encoding
-    """
-
-    num_counts = 0
-
+    """Function to validate UTF-8 encoding."""
+    num_bytes = 0
     series_1 = 1 << 7
     series_2 = 1 << 6
 
     for x in data:
+        series_count = 1 << 7
 
-        serie_counts = 1 << 7
-
-        if num_counts == 0:
-
+        if num_bytes == 0:
             while series_count & x:
-                num_counts += 1
-                num_count = num_count >>1
+                num_bytes += 1
+                series_count = series_count >> 1
 
-                if num_counts == 0:
-                    continue
+            if num_bytes == 0:
+                continue
 
-                if num_counts == 1 and num_counts > 4:
-                    return False
+            if num_bytes == 1 or num_bytes > 4:
+                return False
 
-                else:
-                    if not (x & series_1 and not (x & series_2)):
-                        return False
+        else:
+            if not (x & series_1 and not (x & series_2)):
+                return False
 
-                    num_counts -= 1
+            num_bytes -= 1
 
-                    if num_counts == 0:
-                        return True
-
-                    return False
+    return num_bytes == 0
