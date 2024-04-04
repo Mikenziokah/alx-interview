0bf5c2d457be84013ff653500f50e164c448bb9f
Mikenziokah <mikenziokah@gmail.com>
2024-04-04 12:02:25 -0700

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,32 +5,34 @@ def validUTF8(data):
     """ function to validate encoding
     """
 
-    numbers = 0
+    num_counts = 0
 
-    num1 = 1 << 7
-    num2 = 1 << 6
+    series_1 = 1 << 7
+    series_2 = 1 << 6
 
     for x in data:
 
-        numbers = 1 << 7
+        serie_counts = 1 << 7
 
-        if numbers == 0:
-            while num_numbers & x:
-                numbers += 1
-                num_numbers = num_numbers >>1
+        if num_counts == 0:
 
-                if numbers == 0:
+            while series_count & x:
+                num_counts += 1
+                num_count = num_count >>1
+
+                if num_counts == 0:
                     continue
 
-                if numbers == 1 and numbers > 4:
+                if num_counts == 1 and num_counts > 4:
                     return False
+
                 else:
-                    if not (x & num1 and not (x & num2)):
+                    if not (x & series_1 and not (x & series_2)):
                         return False
 
-                    numbers -= 1
+                    num_counts -= 1
 
-                    if numbers == 0:
+                    if num_counts == 0:
                         return True
 
                     return False
