feb364f3ae2773ca2e6fce3fb0ad19254c6c43a2
Mikenziokah <mikenziokah@gmail.com>
2024-05-09 18:52:49 +0000

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,8 +14,7 @@ def island_perimeter(grid):
     rows, cols = len(grid), len(grid[0])
     for i in range(rows):
         for j in range(cols):
-            if grid[i][j] == 1:
-                
+            if grid[i][j] == 1: 
                 if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
                 if i == rows-1 or grid[i+1][j] == 0:
