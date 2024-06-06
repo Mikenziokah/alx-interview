1f9310f64a31bc46230bc52a52cbbe7e02710c89
Mikenziokah <mikenziokah@gmail.com>
2024-06-06 10:24:13 -0700

    making change code base

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,7 +12,7 @@ def makeChange(coins, total):
     if not coins or coins is None:
         return -1
     if total <= 0:
-        return 0
+        return -1
     change = 0
     coins = sorted(coins)[::-1]
     for coin in coins:
